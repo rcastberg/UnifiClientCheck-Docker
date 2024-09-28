492a460d38170737fbcf178a8d4e675aa4e03c13
Rene <Rene@Castberg.org>
2024-09-28 08:35:12 +0200

    Added always notify for Guest network

--- src/Main.php
+++ src/Main.php
@@ -27,6 +27,7 @@ function getMacAddresses() {
   
 // Environment configuration
 $envKnownMacs =  getMacAddresses();
+$alwaysNotifyGuest = getenv('ALWAYS_NOTIFY_GUEST') ?: '';
 $checkInterval = getenv('CHECK_INTERVAL') ?: 60;
 $notificationService = getenv('NOTIFICATION_SERVICE') ?: 'Telegram';
 $alwaysNotify = filter_var(getenv('ALWAYS_NOTIFY') ?: False, FILTER_VALIDATE_BOOLEAN);
@@ -92,12 +93,19 @@ while (true) {
 
         foreach ($clients as $client) {
             $isNewDevice = !in_array($client->mac ?? $client->id, $knownMacs);
+            $networkProperty = $teleportNotifications ? 'network_name' : 'network';
             if ($isNewDevice) {
                 echo "New device found. Sending a notification.\n";
                 $newDeviceFound = true;
             }
-
-            if ($alwaysNotify || $isNewDevice) {
+            if ($alwaysNotifyGuest && strlen($alwaysNotifyGuest) > 0 && strpos(($client->{$networkProperty} ?? 'N/A'), $alwaysNotifyGuest) !== false) {
+                    echo "Guest device found. Sending a notification.\n";
+                    $NotifyGuest = true;
+            }
+            else {
+                $NotifyGuest = false;
+            }
+            if ($alwaysNotify || $isNewDevice || $NotifyGuest) {
                 if ($teleportNotifications && isset($client->type) && $client->type == 'TELEPORT') {
                     // Format message for Teleport device
                     $message = "Teleport device seen on network:\n";
@@ -105,7 +113,6 @@ while (true) {
                     $message .= "IP Address: " . $client->ip . "\n";
                     $message .= "ID: " . $client->id . "\n";
                 } else {
-					$networkProperty = $teleportNotifications ? 'network_name' : 'network';
                     // Format message for regular device
                     $message = "Device seen on network:\n";
                     $message .= "Device Name: " . ($client->name ?? 'Unknown') . "\n";
